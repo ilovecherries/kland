fb3c5e6f807e745be5a9029832ecacb89a2888df
Cherry <cherry@debian-BULLSEYE-live-builder-AMD64>
2021-10-07 11:34:45 -0400

    going to convert to flask

--- src/kland.nim
+++ src/kland.nim
@@ -2,6 +2,7 @@
 # uses this file as the main entry point of the application.
 
 import jester, htmlgen
+import system
 
 import norm/[sqlite]
 import options
@@ -28,9 +29,9 @@ routes:
   # this is just for a test
   get re"^\/post/([0-9]+)$":
     let id = parseInt(request.matches[0])
-    var post = @[newPost()]
-    dbConn.select(post, "Post.id = ?", id)
-    for i in post:
+    var posts = @[newPost()]
+    dbConn.select(posts, "Post.id = ?", id)
+    for i in posts:
       echo i[]
     resp fmt"check console"
     
@@ -38,39 +39,44 @@ routes:
     let thread = parseInt(request.matches[0])
     resp fmt"This is thread {thread}"
 
-  # i kind of forgot that this ID is supposed to be the thread number agh
-  # post magically not existing...
-  post re"^\/threads/([0-9]+)$":
-    let threadId = parseInt(request.matches[0])
+  # create new thread
+  post "/threads/":
     var data: JsonNode
     try:
       data = parseJson(@"payload")
-      
-      if not data.hasKey("content"):
-        resp Http400, "The content is missing from the payload."
-      
-      let content = data["content"].getStr()
-      # we should sanitize the content first beforehand so that we can detect
-      # garbage such as ZWSP so that it's impossible to make empty messages?
-      # (OR NOT, ZWSP and making you think you're tricking the system is kind
-      #  of fun lol)
-      if content.len == 0:
-        resp Http400, "The content is empty."
+      var (thread, post) = newThreadFromJSON(data)
+      dbConn.insert thread
+      # does this get modified? is that why it needs to be a var?
+      echo thread.id
 
+    except ModelCreateError:
+      resp Http400, getCurrentExceptionMsg()
+    except JsonParsingError:
+      echo getCurrentExceptionMsg()
+      resp Http400, "Invalid JSON."
+    except:
+      resp Http400, "Unknown JSON parsing error."
+
+  # create new post in thread
+  post re"^\/threads/([0-9]+)$":
+    let threadId = parseInt(request.matches[0])
+    # check if the thread exists in this block
+    block:
       if threadId < 1:
         resp Http400, "The thread ID must be larger than zero."
       
-      # we should check if the thread exists
       var threads = @[newThread()]
-      dbConn.select(post, "Thread.id = ?", threadId)
+      dbConn.select(threads, "Thread.id = ?", threadId)
       if threads.len() == 0:
         resp Http400, fmt"The thread ID {threadId} does not exist"
 
-      let author: Option[string] =
-        if data.hasKey("author"): data["author"].getStr() else: none
-
-      dbConn.insert newPost(content, threadId)
-      
+    var data: JsonNode
+    try:
+      data = parseJson(@"payload")
+      var p = newPostFromJSON(threadId, data)
+      dbConn.insert p
+    except ModelCreateError:
+      resp Http400, getCurrentExceptionMsg()
     except JsonParsingError:
       resp Http400, "Invalid JSON."
     except:
